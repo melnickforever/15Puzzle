1a1da5533d20e8ea8ad70bc4b4050f35c01e201d
Dmitry Melnick <melnickforever@gmail.com>
2012-01-29 12:29:43 +0200

    small changes

--- puzzle.rb
+++ puzzle.rb
@@ -12,7 +12,7 @@ class Puzzle
   def initialize
     @solution = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,0]]
     @data = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,0]]
-    @limit = 500
+    @limit = 200
    
     self.set_task
    
